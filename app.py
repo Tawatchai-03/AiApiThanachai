c38f615b6f2b1408834e62870ce6a5fc3f05e318
Tawatchai-03 <tawatchai.kee@rmutto.ac.th>
2024-09-19 16:17:19 +0700

    2

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def laptop():
         return {'error': 'Invalid input values'}, 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=4000)
+    app.run(debug=True, host='0.0.0.0', port=3000)
